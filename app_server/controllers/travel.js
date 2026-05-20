34742a5522560e93341fdcde88894a57284e8a85
Barbara Kendall <barbarakendall@Mac.lan>
2026-05-20 17:25:38 -0400

    Complete Module 3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,17 +1,27 @@
-module.exports = {
-  index: (req, res) => {
+const trips = require('../data/trips.json');
+
+const index = (req, res) => {
     res.render('index', { title: 'Travlr Getaways' });
-  },
+};
 
-  about: (req, res) => {
+const about = (req, res) => {
     res.render('about', { title: 'About Travlr Getaways' });
-  },
+};
 
-  contact: (req, res) => {
+const contact = (req, res) => {
     res.render('contact', { title: 'Contact Travlr Getaways' });
-  }, 
+};
 
-  travel: (req, res) => {
-  res.render('travel', { title: 'Travel' });
-}
+const travel = (req, res) => {
+    res.render('travel', {
+        title: 'Travlr Getaways',
+        trips
+    });
+};
+
+module.exports = {
+    index,
+    about,
+    contact,
+    travel
 };
